dbb8da9e08ebc7a596be88dc63315e7bfcddab47
hossein.objectj <hossein.objectj@0a2aaa40-c41f-11dd-8f28-a1c7f7561c61>
2008-12-12 22:08:41 +0000

    major fixes

--- java/alfresco/custom_model/lookup_from_external_url/src/sample/model/constraint/Sites.java
+++ java/alfresco/custom_model/lookup_from_external_url/src/sample/model/constraint/Sites.java
@@ -15,42 +15,53 @@ import org.apache.log4j.Logger;
 
 public class Sites extends ListOfValuesConstraint implements Serializable {
 
-    private static final long serialVersionUID = 1;
-    private static Logger log = Logger.getLogger(Sites.class);
+	private static final long serialVersionUID = 1;
+	private static Logger log = Logger.getLogger(Sites.class);
 
-    private String sourceUrl;
-    
-    /** TODO: if you remove this method, type dpesn't show up !! */
-    public void initialize() {
-    }
+	private String sourceUrl;
 
-    @Override
-    public List<String> getAllowedValues() {
-        List<String> av = new ArrayList<String>();
-        try {
-            URL url = new URL(this.getSourceUrl());
-            URLConnection urlConnection = url.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null && inputLine.trim().length() != 0) {
-                if (log.isDebugEnabled())
-                    log.debug(inputLine);
-                av.add(inputLine);
-            }
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // you need to set the super class, apparently in super class they used the vairiable directly 
-        super.setAllowedValues(av);
-        return av;
-    }
+	/** TODO: if you remove this method, type dpesn't show up !! */
+	public void initialize() {
+	}
 
-    public String getSourceUrl() {
-        return sourceUrl;
-    }
+	@Override
+	public List<String> getAllowedValues() {
+		List<String> av = new ArrayList<String>();
+		try {
+			URL url = new URL(this.getSourceUrl());
+			URLConnection urlConnection = url.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null && inputLine.trim().length() != 0) {
+				if (log.isDebugEnabled())
+					log.debug(inputLine);
+				av.add(inputLine);
+			}
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// you need to set the super class, apparently in super class they used
+		// the vairiable directly
+		super.setAllowedValues(av);
+		return av;
+	}
 
-    public void setSourceUrl(String sourceUrl) {
-        this.sourceUrl = sourceUrl;
-    }
+	/**
+	 * Set the values that are allowed by the constraint.
+	 * 
+	 * @param values
+	 *            a list of allowed values
+	 */
+	@SuppressWarnings("unchecked")
+	public void setAllowedValues(List allowedValues) {
+	}
+
+	public String getSourceUrl() {
+		return sourceUrl;
+	}
+
+	public void setSourceUrl(String sourceUrl) {
+		this.sourceUrl = sourceUrl;
+	}
 }
